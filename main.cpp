fd2fc1af27b067228b42ef0e48bae42cd9cc3f88
Ray <rayliu672@gmail.com>
2022-07-03 18:48:38 -0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <adder.h>
+
+
+int main() {
+
+    std::cout << "Hello World << \n";
+    // floats must include f behind the number
+    std::cout << add(70.1f, 73.1f) << '\n';
+
+    return 0;
+}
